029375e60579e1a82001c649e2d54a59690a1eb3
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-08 16:12:04 -0400

    Style PortfolioItems

--- src/components/portfolioItems.js
+++ src/components/portfolioItems.js
@@ -1,5 +1,22 @@
 import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
+import styled from 'styled-components'
+
+const PortfolioItemsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const PortfolioItem = styled.div`
+  width: 300px;
+  border: 1px solid #efefef;
+  padding: 16px;
+  margin: 16px;
+`;
+
+const PortfolioImage = styled.img`
+  max-width: 100%;
+`;
 
 const PortfolioItems = () => {
 
@@ -23,14 +40,17 @@ const PortfolioItems = () => {
           }
         }
       }
-    `} render={props => props.allWordpressWpPortfolio.edges.map(portfolioItem => (
-        <div key={portfolioItem.node.id}>
+    `} render={props => 
+        (<PortfolioItemsWrapper>
+        {props.allWordpressWpPortfolio.edges.map(portfolioItem => (
+        <PortfolioItem key={portfolioItem.node.id}>
             <h2>{portfolioItem.node.title}</h2>
-            <img src={portfolioItem.node.featured_media.source_url} alt="portfolio thumbnail" />
+            <PortfolioImage src={portfolioItem.node.featured_media.source_url} alt="portfolio thumbnail" />
             <div dangerouslySetInnerHTML={{__html: portfolioItem.node.excerpt}} />
             <Link to={`portfolio/${portfolioItem.node.slug}`}>Read more</Link>
-        </div>
-    ))}/>
+        </PortfolioItem>))}
+        </PortfolioItemsWrapper>
+    )} />
   );
 }
 
