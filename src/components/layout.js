c6c5772170e398424c79ce2df88444d428c8bc8d
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-10 14:54:34 -0400

    Add favicon with graphql query and react helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,8 +7,9 @@
 
 import React from "react"
 import MainMenu from './mainMenu'
-
+import {Helmet} from 'react-helmet'
 import styled, { createGlobalStyle } from 'styled-components'
+import { useStaticQuery, graphql } from 'gatsby'
 
 const GlobalStyles = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap');
@@ -25,15 +26,40 @@ const LayoutWrapper = styled.div`
   margin: 0 auto;
 `;
 
-const Layout = ({ children }) => (
+const Layout = ({ children }) => {
+  
+  const data = useStaticQuery(graphql`
+  {
+    allWordpressWpFavicon {
+      edges {
+        node {
+          url {
+            alt_text
+            source_url
+          }
+        }
+      }
+    }
+  }
+  `);
+  
+  console.log('Layout, data:', data);
+
+  return (
   <div>
+    <Helmet>
+      <title>My Awesome Portfolio</title>
+      <meta name="My Awesome Portfolio" content="My Awesome Portfolio" />
+      <meta charSet="utf-8" />
+      <link rel="icon" type="image/png" href={data.allWordpressWpFavicon.edges[0].node.url.source_url} sizes="16x16" />
+    </Helmet>
     <GlobalStyles />
     <MainMenu />
     <LayoutWrapper>
       {children}
     </LayoutWrapper>
   </div>
-)
+)}
 
 
 export default Layout
