1cf23495fb08888fdd51e0d4a0e4463500dead71
Jacob A. Carpenter <jacquedalus@gmail.com>
2020-07-20 16:03:42 -0400

    Fix noreferrer warning, fix FeaturedImage UI bug

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -1,57 +1,64 @@
-import React, { Component } from "react"
+import React from "react"
 import { graphql, StaticQuery, Link } from "gatsby"
-import styled from 'styled-components'
-import SiteInfo from './siteInfo'
+import styled from "styled-components"
+import SiteInfo from "./siteInfo"
 
 const MainMenuWrapper = styled.div`
-    display: flex;
-    
-    background-color: rgb(3, 27, 77);
-    padding: 10px 0;
-`;
+  display: flex;
+
+  background-color: rgb(3, 27, 77);
+  padding: 10px 0;
+`
 
 const MenuItem = styled(Link)`
-    color: white;
-    display: block;
-    padding: 8px 16px;
-`;
+  color: white;
+  display: block;
+  padding: 8px 16px;
+`
 
 const MainMenuInner = styled.div`
-    display: flex;
-    align-items: center;
-    width: 960px;
-    height: 100%;
-    max-width: 960px;
-    margin: 0 auto;
-`;
+  display: flex;
+  align-items: center;
+  width: 960px;
+  height: 100%;
+  max-width: 960px;
+  margin: 0 auto;
+`
 
 const MainMenu = () => (
-    <StaticQuery query={graphql`
-    {
-        allWordpressWpApiMenusMenusItems(filter: {name: {eq: "Main menu"}})  {
-            edges {
-                node {
-                    name
-                    items {
-                        title
-                        object_slug
-                    }
-                }
+  <StaticQuery
+    query={graphql`
+      {
+        allWordpressWpApiMenusMenusItems(
+          filter: { name: { eq: "Main menu" } }
+        ) {
+          edges {
+            node {
+              name
+              items {
+                title
+                object_slug
+              }
             }
+          }
         }
-    }
-    `} render={props => (
-        <MainMenuWrapper>
-            <MainMenuInner>
-            <SiteInfo />
-            {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => (
-                <MenuItem to={item.object_slug} key={item.title}>
-                    {item.title}
-                </MenuItem>
-            ))}
-            </MainMenuInner>
-        </MainMenuWrapper>
-    )} />
-);
+      }
+    `}
+    render={props => (
+      <MainMenuWrapper>
+        <MainMenuInner>
+          <SiteInfo />
+          {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(
+            item => (
+              <MenuItem to={item.object_slug} key={item.title}>
+                {item.title}
+              </MenuItem>
+            )
+          )}
+        </MainMenuInner>
+      </MainMenuWrapper>
+    )}
+  />
+)
 
-export default MainMenu;
+export default MainMenu
--- src/templates/portfolio.js
+++ src/templates/portfolio.js
@@ -11,10 +11,17 @@ export default ({ pageContext }) => (
   <Layout>
     <h1>{pageContext.title}</h1>
     <strong>Website url: </strong>
-    <a target="_blank" href={pageContext.acf.portfolio_url}>
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={pageContext.acf.portfolio_url}
+    >
       {pageContext.acf.portfolio_url}
     </a>
-    <FeaturedImage src={pageContext.featured_media.source_url} />
+
+    <div>
+      <FeaturedImage src={pageContext.featured_media.source_url} />
+    </div>
     <div dangerouslySetInnerHTML={{ __html: pageContext.content }} />
   </Layout>
 )
